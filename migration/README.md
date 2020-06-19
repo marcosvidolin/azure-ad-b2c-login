3489e3f8edab62ea8c3d5d9de0fe7e6e2ab24501
Marcos Vidolin <marcosvidolin@gmail.com>
2020-06-19 00:50:10 -0300

    chore: initial commit

---
+++ migration/README.md
@@ -0,0 +1,3 @@
+# User Migration
+
+This is a migration application to import users from a legacy IdP to the Azure AD B2C.
---
+++ migration/src/main/java/com/vidolima/userimport/UserimportApplication.java
@@ -0,0 +1,13 @@
+package com.vidolima.userimport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserimportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserimportApplication.class, args);
+	}
+
+}
